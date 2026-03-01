d006dfc7da34e119ba662a5685a532b79dcf89ad
Juancitopepe <1pepitojuanvladsc@gmail.com>
2026-03-01 14:01:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const { Client, GatewayIntentBits } = require("discord.js");
+const express = require("express");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`🌐 Servidor web activo en puerto ${PORT}`);
+});
+
+// BOT DISCORD
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
